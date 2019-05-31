f9d4fd07a27bb185806df3be0dc70ff68e02277e
PACHINGONA14 <51245542+PACHINGONA14@users.noreply.github.com>
2019-05-31 13:57:46 -0500

    FINAL_PROJECT

---
+++ Beautiful-Patterns-master/dashboard.js
@@ -0,0 +1,50 @@
+/* globals Chart:false, feather:false */
+
+(function () {
+  'use strict'
+
+  feather.replace()
+
+  // Graphs
+  var ctx = document.getElementById('myChart')
+  // eslint-disable-next-line no-unused-vars
+  var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [
+        'Racism',
+        'Gender',
+        'Sexual Oriemtation',
+        'Religion',
+        'Apperiance',
+      ],
+      datasets: [{
+        data: [
+          49,
+          34,
+          44,
+          24,
+          56,
+          
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
+        borderWidth: 4,
+        pointBackgroundColor: '#007bff'
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
+        }]
+      },
+      legend: {
+        display: false
+      }
+    }
+  })
+})()
